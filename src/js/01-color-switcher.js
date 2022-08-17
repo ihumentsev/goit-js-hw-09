1a0c50fd300868274e570c20bc601d3d890b3e19
ihumentsev <a.ihumentsev@gmail.com>
2022-08-17 23:00:01 +0200

    add-first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
 
+refs.startBtn.addEventListener('click', onBtnStarted);
+refs.stopBtn.addEventListener('click', onBtnStoped);
+
+function onBtnStarted() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+}
+
+function onBtnStoped() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+}
