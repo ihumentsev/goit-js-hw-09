de0bbda5b2ffe48a7caf2779999c44dd54c01a74
ihumentsev <a.ihumentsev@gmail.com>
2022-08-18 23:14:23 +0200

    ...

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,15 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+////////////////////
+const btnEl = document.querySelector('.submit');
+
+btnEl.addEventListener('submit', event => {
+  event.preventDefault();
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      Notify.success('Sol lucet omnibus');
+    } else {
+      Notify.failure('Qui timide rogat docet negare');
+    }
   }
-}
+});
